937840ab754ee613daf11843c330a9f07d086315
MariyaKud <m.kudryashova@tealfox.com>
2023-08-08 11:06:52 +0300

    Add recommendations

--- src/main/java/ru/yandex/practicum/filmorate/repository/DatabaseRecommendationRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/DatabaseRecommendationRepository.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
+
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.RatingMpa;
