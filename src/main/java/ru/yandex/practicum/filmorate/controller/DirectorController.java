4583df4805150dc60f16448ac087f470b45ad8a4
Vladislav <justnik01@yandex.ru>
2023-08-09 17:05:17 +0300

    fix: сортировка по жанру в одном запросе + правки по ревью

--- src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Director;
 import ru.yandex.practicum.filmorate.service.DirectorService;
@@ -14,7 +13,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/directors")
 @RequiredArgsConstructor
-@Validated
 public class DirectorController {
 
     private final DirectorService directorService;
